461bee32dac3c2723237f751e0267c4e163f529a
glanctot <gregory.lanctot28@gmail.com>
2022-03-14 09:36:13 -0500

    cleanup

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: saveBookData): User
+        removeBook(bookId: ID!): User
+    }
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input saveBookData {
+        authors: [String]
+        title: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+    }
+`;
+
+module.exports = typeDefs;
